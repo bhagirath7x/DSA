6b59e2c3ae7f8601e580e0f55866d7c4861d9462
Bhagirath <134389175+bhagirath7x@users.noreply.github.com>
2026-03-10 11:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,45 +1,9 @@
 class Solution {
 public:
-    void helper(vector<int>& arr, int start, int mid, int end) {
-        vector<int>temp(end-start+1);
-        int left = start, right = mid+1, idx = 0;
-        while(left <= mid && right <= end) {
-            if(arr[left] <= arr[right]) {
-                temp[idx] = arr[left];
-                idx++, left++;
-            }
-            else {
-                temp[idx] = arr[right];
-                idx++, right++;
-            }
-        }
-        while(left <= mid) {
-            temp[idx] = arr[left];
-            idx++, left++;
-        }
-        while(right <= end) {
-            temp[idx] = arr[right];
-            idx++, right++;
-        }
-        idx = start;
-        for(int i = 0; i < temp.size(); i++) {
-            arr[idx] = temp[i];
-            idx++;
-        }
-    }
-    void mergeSort(vector<int>& arr, int start, int end) {
-        if(start == end) return;
-        int mid = start + (end - start) / 2;
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid+1, end);
-
-        helper(arr, start, mid, end);
-    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = 0; i < nums2.size(); i++) {
+        for(int i = 0; i < n; i++) {
             nums1[m+i] = nums2[i];
         }
-
-        mergeSort(nums1, 0, nums1.size()-1);
+        sort(nums1.begin(), nums1.end());
     }
 };
