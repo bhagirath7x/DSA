3258dadb5b6e3d6f5a40d0099a8297f7dc32d7ee
Bhagirath <134389175+bhagirath7x@users.noreply.github.com>
2026-07-23 15:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (5.12%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.size()-1;
-        
-        while(i >= 0 && s[i] == ' ') {
-            i--;
-        }
-        int count = 0;
-        while(i >= 0 && s[i] != ' ') {
-            count++;
-            i--;
+
+        stringstream ss(s);
+
+        string token;
+
+        while (ss >> token) {
+            // the last word will stored in tkn
         }
 
-        return count;
+        return token.length();
     }
 };
